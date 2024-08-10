04ab4553c720618dfe3186100fed6101389f03a5
kyrex32 <138849020+kyrex32@users.noreply.github.com>
2024-08-10 11:01:19 +0300

    index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let client = new Discord.Client()
 
 
 client.on('ready', () => {
- client.user.setActivity(`1.0.1 your prefix here`, { type: 'PLAYING'})
+ client.user.setActivity(`🟢 Paying Rewards - Online`, { type: 'WATCHING'})
 })
 
 client.login(process.env.token);
